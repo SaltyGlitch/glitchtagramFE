dc88140ec9f5ae5a5e61359fa2d9dbce10600f87
SAMET KURT <samet.krt@gmail.com>
2023-03-21 13:15:00 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello it is me!!</p>
-    </div>
-  );
+interface WeatherForecast {
+  date: Date;
+  temperatureC: number;
+  temperatureF: number;
+  summary?: string;
 }
-
+function App() {
+  const [data, setData] = useState<WeatherForecast[]>([]);
+  const fetchingdata = async () => {
+    const response = await fetch(
+      "https://glitchtagramcli.azurewebsites.net/WeatherForecast"
+    );
+    const result = await response.json();
+    console.log(result);
+  };
+  fetchingdata();
+  return <>
+  <p>Hello, It is me!!</p>
+  <p>Hello, It is you!!</p>
+ 
+  </>;
+}
 export default App;
