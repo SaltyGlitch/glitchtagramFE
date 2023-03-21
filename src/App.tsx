caf6ddffd3c20235f35c0329d3b8d6625e154478
SAMET KURT <samet.krt@gmail.com>
2023-03-21 13:46:28 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-// interface WeatherForecast {
-//   date: Date;
-//   temperatureC: number;
-//   temperatureF: number;
-//   summary?: string;
-// }
+interface WeatherForecast {
+  date: Date;
+  temperatureC: number;
+  temperatureF: number;
+  summary?: string;
+}
 function App() {
-  // const [data, setData] = useState<WeatherForecast[]>([]);
-  const fetchingdata = async () => {
-    const response = await fetch(
-      "https://glitchtagramcli.azurewebsites.net/WeatherForecast"
-    );
-    const result = await response.json();
-    console.log(result);
+  // // const [data, setData] = useState<WeatherForecast[]>([]);
+  // const fetchingdata = async () => {
+  //   const response = await fetch(
+  //     "https://glitchtagramcli.azurewebsites.net/weatherforecast"
+  //   );
+  //   const result = await response.json();
+  //   console.log(result);
+  // };
+  // fetchingdata();
+
+  const apiCall = async () => {
+    try {
+      const URL = "https://glitchtagramcli.azurewebsites.net/weatherforecast";
+      const config = {
+        method: "GET",
+        mode: "no-cors",
+        cache: "default",
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          Authorization: "Token  *****************",
+        },
+      };
+
+      const { data } = await axios.get(URL, config);
+      // setAllDataEvents(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
-  fetchingdata();
+
+  useEffect(() => {
+    apiCall();
+  },[])
+
+  
+  
   return <>
   <p>Hello, It is me!!</p>
   <p>Hello, It is you!!</p>
