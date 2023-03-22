8b49a26e69464ec120554c07cd7e5586142490ae
SAMET KURT <samet.krt@gmail.com>
2023-03-22 21:58:26 +0100

    Move delete and list function to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,94 @@
-import React, {useEffect} from 'react';
-import './App.css';
-import axios from 'axios';
-import FileUploadSingle from "./components/FileUploadSingle";
-import ListAllImages from './components/ListAllImages';
+// import React, {useEffect} from 'react';
+// import './App.css';
+// import axios from 'axios';
+// import FileUploadSingle from "./components/FileUploadSingle";
+// import ListAllImages from './components/ListAllImages';
 
-// interface WeatherForecast {
-//   date: Date;
-//   temperatureC: number;
-//   temperatureF: number;
-//   summary?: string;
+// function App() {
+
+//     const apiCall = async () => {
+//         try {
+//             const URL = "https://glitchtagramcli.azurewebsites.net/weatherforecast";
+//             const config = {
+//                 method: "GET",
+//                 mode: "no-cors",
+//                 cache: "default",
+//                 headers: {
+//                     "Access-Control-Allow-Headers": "Content-Type",
+//                     "Access-Control-Allow-Origin": "*",
+//                     Authorization: "Token  *****************",
+//                 },
+//             };
+
+//             const {data} = await axios.get(URL, config);
+//             // setAllDataEvents(data);
+//             console.log(data);
+//         } catch (err) {
+//             console.log(err);
+//         }
+//     };
+
+//     useEffect(() => {
+//         apiCall();
+//     }, [])
+
+
+//     return <>
+//         <p>Hello, It is me!!</p>
+//         <p>Hello, It is you!!</p>
+//         <FileUploadSingle/>
+//         <ListAllImages/>
+//     </>;
 // }
-function App() {
-    // // const [data, setData] = useState<WeatherForecast[]>([]);
-    // const fetchingdata = async () => {
-    //   const response = await fetch(
-    //     "https://glitchtagramcli.azurewebsites.net/weatherforecast"
-    //   );
-    //   const result = await response.json();
-    //   console.log(result);
-    // };
-    // fetchingdata();
 
-    const apiCall = async () => {
-        try {
-            const URL = "https://glitchtagramcli.azurewebsites.net/weatherforecast";
-            const config = {
-                method: "GET",
-                mode: "no-cors",
-                cache: "default",
-                headers: {
-                    "Access-Control-Allow-Headers": "Content-Type",
-                    "Access-Control-Allow-Origin": "*",
-                    Authorization: "Token  *****************",
-                },
-            };
+// export default App;
 
-            const {data} = await axios.get(URL, config);
-            // setAllDataEvents(data);
-            console.log(data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
+import React, { useEffect, useState } from "react";
+import ListAllImages from "./components/ListAllImages";
+import "./App.css";
+import FileUploadSingle from "./components/FileUploadSingle";
 
-    useEffect(() => {
-        apiCall();
-    }, [])
+const App = () => {
+  const [images, setImages] = useState<string[]>([]);
 
+  const fetchingImages = async () => {
+    const response = await fetch(
+      "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
+      // "https://localhost:7244/Pictures/getimages"
+    );
+    const result = await response.json();
+    console.log(result);
+    setImages(result);
+  };
 
-    return <>
-        <p>Hello, It is me!!</p>
-        <p>Hello, It is you!!</p>
-        <FileUploadSingle/>
-        <ListAllImages/>
-    </>;
-}
+  const deleteImage = async (image: string) => {
+    const fileName = image.split("/").pop();
+    const response = await fetch(
+      `https://glitchtagramcli.azurewebsites.net/Pictures/delete/${fileName}`,
+      {
+        method: "DELETE",
+      }
+      // `https://localhost:7244/Pictures/delete/${fileName}`,
+      // {
+      //   method: "DELETE",
+      // }
+    );
+    const result = await response.json();
+    console.log(result);
+    setImages(images.filter((item) => item !== image));
+  };
+
+  useEffect(() => {
+    fetchingImages();
+  }, []);
+
+  return (
+    <div>
+      <FileUploadSingle />
+      <ListAllImages images={images} deleteImage={deleteImage} />
+
+    </div>
+  );
+};
 
 export default App;
--- src/components/ListAllImages.tsx
+++ src/components/ListAllImages.tsx
@@ -1,50 +1,77 @@
-import React, { useEffect, useState } from "react";
-import './ListAllImages.css';
+// import React, { useEffect, useState } from "react";
+// import './ListAllImages.css';
 
-const ListAllImages = () => {
-  const [images, setImages] = useState<string[]>([]);
+// const ListAllImages = () => {
+//   const [images, setImages] = useState<string[]>([]);
 
-  const fetchingImages = async () => {
-    const response = await fetch(
-      "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
-    // "https://localhost:7244/Pictures/getimages"
-    );
-    const result = await response.json();
-    console.log(result);
-    setImages(result);
-  };
+//   const fetchingImages = async () => {
+//     const response = await fetch(
+//       "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
+//     // "https://localhost:7244/Pictures/getimages"
+//     );
+//     const result = await response.json();
+//     console.log(result);
+//     setImages(result);
+//   };
   
-  const deleteImage = async (image: string) => {
-    const fileName = image.split('/').pop();
-    const response = await fetch(
-      `https://glitchtagramcli.azurewebsites.net/Pictures/delete/${fileName}`,
-      {
-        method: "DELETE",
-      }
-    // `https://localhost:7244/Pictures/delete/${fileName}`,
-    // {
-    //   method: "DELETE",
-    // }
-    );
-    const result = await response.json();
-    console.log(result);
-    fetchingImages();
-  };
-useEffect(()=>{
-    fetchingImages();
-},[])
+//   const deleteImage = async (image: string) => {
+//     const fileName = image.split('/').pop();
+//     const response = await fetch(
+//       `https://glitchtagramcli.azurewebsites.net/Pictures/delete/${fileName}`,
+//       {
+//         method: "DELETE",
+//       }
+//     // `https://localhost:7244/Pictures/delete/${fileName}`,
+//     // {
+//     //   method: "DELETE",
+//     // }
+//     );
+//     const result = await response.json();
+//     console.log(result);
+//     fetchingImages();
+//   };
+// useEffect(()=>{
+//     fetchingImages();
+// },[])
+//   return (
+//     <div >
+//       {images.map((image) => {
+//         return (
+//           <div key={image}>
+//             <img src={image} alt="" />
+//             <button onClick={() => deleteImage(image)}>Delete Image</button>
+//           </div>
+//         )
+//       })}
+//     </div>
+//   );
+// };
+
+// export default ListAllImages;
+
+import React from "react";
+import "./ListAllImages.css";
+
+interface Props {
+  images: string[];
+  deleteImage: (image: string) => Promise<void>;
+}
+
+const ListAllImages = ({ images, deleteImage }: Props) => {
+    
   return (
-    <div >
+    <div>
       {images.map((image) => {
         return (
           <div key={image}>
             <img src={image} alt="" />
             <button onClick={() => deleteImage(image)}>Delete Image</button>
           </div>
-        )
+        );
       })}
     </div>
   );
 };
 
 export default ListAllImages;
+
