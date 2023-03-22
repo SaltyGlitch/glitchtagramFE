3e2f9f7068f24c0941ba0d4e101ef927d8e70448
Vlad <vlad.anitas@appliedtechnology.se>
2023-03-22 13:37:23 +0100

    created the FilUploadSingle

---
+++ src/components/FileUploadSingle.tsx
@@ -0,0 +1,47 @@
+import React, {useState, ChangeEvent} from "react";
+
+interface PictureProp {
+    description: string;
+    uploadedAt: Date;
+    Likes: number;
+}
+
+const FileUploadSingle = () => {
+    const [file, setFile] = useState<File>();
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    const handleUploadClick = () => {
+        if (!file) {
+            return;
+        }
+
+        // 👇 Uploading the file using the fetch API to the server
+        fetch('https://glitchtagramcli.azurewebsites.net/Pictures/uploadImage', {
+            method: 'POST',
+            body: file,
+            // 👇 Set headers manually for single file upload
+            headers: {
+                'content-type': file.type,
+                'content-length': `${file.size}`, // 👈 Headers need to be a string
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange}/>
+
+            <div>{file && `${file.name} - ${file.type}`}</div>
+
+            <button onClick={handleUploadClick}>Upload</button>
+        </div>
+    );
+}
