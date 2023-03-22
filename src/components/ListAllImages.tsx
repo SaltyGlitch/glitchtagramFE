fa96ae15c8ca67ae7333878ca6f2c5a8ec70b512
SAMET KURT <samet.krt@gmail.com>
2023-03-22 20:21:13 +0100

    Delete function added

--- src/components/ListAllImages.tsx
+++ src/components/ListAllImages.tsx
@@ -1,4 +1,31 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import './ListAllImages.css';
+
+// const ListAllImages = () => {
+//   const [images, setImages] = useState<string[]>([]);
+
+//   const fetchingImages = async () => {
+//     const response = await fetch(
+//       "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
+//     // "https://localhost:7244/Pictures/getimages"
+//     );
+//     const result = await response.json();
+//     console.log(result);
+//     setImages(result);
+//   };
+//   fetchingImages();
+//   return (
+//     <div>
+//       {images.map((image) => {
+//         return <img src={image} alt="" />
+//       })}
+//     </div>
+//   );
+// };
+
+// export default ListAllImages;
+
+import React, { useEffect, useState } from "react";
 import './ListAllImages.css';
 
 const ListAllImages = () => {
@@ -13,11 +40,35 @@ const ListAllImages = () => {
     console.log(result);
     setImages(result);
   };
-  fetchingImages();
+  
+  const deleteImage = async (image: string) => {
+    const fileName = image.split('/').pop();
+    const response = await fetch(
+      `https://glitchtagramcli.azurewebsites.net/Pictures/delete/${fileName}`,
+      {
+        method: "DELETE",
+      }
+    // `https://localhost:7244/Pictures/delete/${fileName}`,
+    // {
+    //   method: "DELETE",
+    // }
+    );
+    const result = await response.json();
+    console.log(result);
+    fetchingImages();
+  };
+useEffect(()=>{
+    fetchingImages();
+},[])
   return (
-    <div>
+    <div >
       {images.map((image) => {
-        return <img src={image} alt="" />
+        return (
+          <div key={image}>
+            <img src={image} alt="" />
+            <button onClick={() => deleteImage(image)}>Delete Image</button>
+          </div>
+        )
       })}
     </div>
   );
