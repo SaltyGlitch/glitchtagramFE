5c6c7d2691909567f888d1ab26822faeca55af46
SAMET KURT <samet.krt@gmail.com>
2023-03-22 16:36:51 +0100

    List all images

--- src/components/ListAllImages.tsx
+++ src/components/ListAllImages.tsx
@@ -1,26 +1,24 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 const ListAllImages = () => {
-  const [images,setImages] = useState<string[]>([]);
-  
-    const fetchingImages = async () => {
-      const response = await fetch(
-        "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
-      );
-      const result = await response.json();
-      console.log(result);
-      setImages(result);
-    };
-    fetchingImages();
-    return (
-    <div>
-      <ul>
-        
-      </ul>
-      <img src={images[2]} alt="" />
-    </div>
-  )
-}
+  const [images, setImages] = useState<string[]>([]);
 
-export default ListAllImages
+  const fetchingImages = async () => {
+    const response = await fetch(
+      "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
+    );
+    const result = await response.json();
+    console.log(result);
+    setImages(result);
+  };
+  fetchingImages();
+  return (
+    <div>
+      {images.map((image) => {
+        return <img src={image} alt="" />;
+      })}
+    </div>
+  );
+};
+
+export default ListAllImages;
