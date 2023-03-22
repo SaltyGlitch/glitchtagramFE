3d8929e8ef1b4379646588dd5b31d739ec9a6e6a
Vlad <vlad.anitas@appliedtechnology.se>
2023-03-22 14:13:44 +0100

    changed the formData : image -> pictures

--- src/components/FileUploadSingle.tsx
+++ src/components/FileUploadSingle.tsx
@@ -1,49 +1,6 @@
 import React, {useState, ChangeEvent, FormEvent} from "react";
 
-// interface PictureProp {
-//     description: string;
-//     uploadedAt: Date;
-//     Likes: number;
-// }
-
 const FileUploadSingle = () => {
-    // const [file, setFile] = useState<File>();
-    //
-    // const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    //     if (e.target.files) {
-    //         setFile(e.target.files[0]);
-    //     }
-    // };
-    //
-    // const handleUploadClick = () => {
-    //     if (!file) {
-    //         return;
-    //     }
-    //
-    //     // 👇 Uploading the file using the fetch API to the server
-    //     fetch('https://glitchtagramcli.azurewebsites.net/Pictures/uploadImage', {
-    //         method: 'POST',
-    //         body: file,
-    //         // 👇 Set headers manually for single file upload
-    //         headers: {
-    //             'content-type': file.type,
-    //             'content-length': `${file.size}`, // 👈 Headers need to be a string
-    //         },
-    //     })
-    //         .then((res) => res.json())
-    //         .then((data) => console.log(data))
-    //         .catch((err) => console.error(err));
-    // };
-    //
-    // return (
-    //     <div>
-    //         <input type="file" onChange={handleFileChange}/>
-    //
-    //         <div>{file && `${file.name} - ${file.type}`}</div>
-    //
-    //         <button onClick={handleUploadClick}>Upload</button>
-    //     </div>
-    // );
 
     const [file, setFile] = useState<File | null>(null);
 
@@ -57,7 +14,7 @@ const FileUploadSingle = () => {
         if (!file) return;
 
         const formData = new FormData();
-        formData.append('image', file);
+        formData.append('pictures', file);
 
         fetch('https://glitchtagramcli.azurewebsites.net/Pictures/uploadImage', {
             method: 'POST',
