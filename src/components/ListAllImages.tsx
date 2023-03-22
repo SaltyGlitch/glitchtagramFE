37a97d723fb67d257b4b11288c17f7a2a325a1aa
SAMET KURT <samet.krt@gmail.com>
2023-03-22 16:46:40 +0100

    .

--- src/components/ListAllImages.tsx
+++ src/components/ListAllImages.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './ListAllImages.css';
 
 const ListAllImages = () => {
   const [images, setImages] = useState<string[]>([]);
@@ -6,6 +7,7 @@ const ListAllImages = () => {
   const fetchingImages = async () => {
     const response = await fetch(
       "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
+    // "https://localhost:7244/Pictures/getimages"
     );
     const result = await response.json();
     console.log(result);
@@ -15,7 +17,7 @@ const ListAllImages = () => {
   return (
     <div>
       {images.map((image) => {
-        return <img src={image} alt="" />;
+        return <img src={image} alt="" />
       })}
     </div>
   );
