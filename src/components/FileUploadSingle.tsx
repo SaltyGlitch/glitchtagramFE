f3449c6eb4d61e2dc7fbc5e2c0d1f10d1d059d0b
Vlad <vlad.anitas@appliedtechnology.se>
2023-03-22 14:01:19 +0100

    changed the upload file tsx

--- src/components/FileUploadSingle.tsx
+++ src/components/FileUploadSingle.tsx
@@ -1,4 +1,4 @@
-import React, {useState, ChangeEvent} from "react";
+import React, {useState, ChangeEvent, FormEvent} from "react";
 
 // interface PictureProp {
 //     description: string;
@@ -7,42 +7,75 @@ import React, {useState, ChangeEvent} from "react";
 // }
 
 const FileUploadSingle = () => {
-    const [file, setFile] = useState<File>();
+    // const [file, setFile] = useState<File>();
+    //
+    // const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    //     if (e.target.files) {
+    //         setFile(e.target.files[0]);
+    //     }
+    // };
+    //
+    // const handleUploadClick = () => {
+    //     if (!file) {
+    //         return;
+    //     }
+    //
+    //     // 👇 Uploading the file using the fetch API to the server
+    //     fetch('https://glitchtagramcli.azurewebsites.net/Pictures/uploadImage', {
+    //         method: 'POST',
+    //         body: file,
+    //         // 👇 Set headers manually for single file upload
+    //         headers: {
+    //             'content-type': file.type,
+    //             'content-length': `${file.size}`, // 👈 Headers need to be a string
+    //         },
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => console.log(data))
+    //         .catch((err) => console.error(err));
+    // };
+    //
+    // return (
+    //     <div>
+    //         <input type="file" onChange={handleFileChange}/>
+    //
+    //         <div>{file && `${file.name} - ${file.type}`}</div>
+    //
+    //         <button onClick={handleUploadClick}>Upload</button>
+    //     </div>
+    // );
 
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            setFile(e.target.files[0]);
-        }
-    };
+    const [file, setFile] = useState<File | null>(null);
 
-    const handleUploadClick = () => {
-        if (!file) {
-            return;
-        }
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0];
+        setFile(selectedFile || null);
+    }
 
-        // 👇 Uploading the file using the fetch API to the server
+    const handleUpload = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (!file) return;
+
+        const formData = new FormData();
+        formData.append('image', file);
+
         fetch('https://glitchtagramcli.azurewebsites.net/Pictures/uploadImage', {
             method: 'POST',
-            body: file,
-            // 👇 Set headers manually for single file upload
-            headers: {
-                'content-type': file.type,
-                'content-length': `${file.size}`, // 👈 Headers need to be a string
-            },
+            body: formData
         })
-            .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.error(err));
-    };
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
 
     return (
         <div>
-            <input type="file" onChange={handleFileChange}/>
-
-            <div>{file && `${file.name} - ${file.type}`}</div>
-
-            <button onClick={handleUploadClick}>Upload</button>
+            <form onSubmit={handleUpload}>
+                <input type="file" onChange={handleFileChange} />
+                <button type="submit">Upload</button>
+            </form>
         </div>
     );
+    
 }
 export default FileUploadSingle;
