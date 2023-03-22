894d124b812e4cdab1f995b4b19e30a99470cd70
SAMET KURT <samet.krt@gmail.com>
2023-03-22 20:30:24 +0100

    Add delete function

--- src/components/ListAllImages.tsx
+++ src/components/ListAllImages.tsx
@@ -1,30 +1,3 @@
-// import React, { useState } from "react";
-// import './ListAllImages.css';
-
-// const ListAllImages = () => {
-//   const [images, setImages] = useState<string[]>([]);
-
-//   const fetchingImages = async () => {
-//     const response = await fetch(
-//       "https://glitchtagramcli.azurewebsites.net/Pictures/getimages"
-//     // "https://localhost:7244/Pictures/getimages"
-//     );
-//     const result = await response.json();
-//     console.log(result);
-//     setImages(result);
-//   };
-//   fetchingImages();
-//   return (
-//     <div>
-//       {images.map((image) => {
-//         return <img src={image} alt="" />
-//       })}
-//     </div>
-//   );
-// };
-
-// export default ListAllImages;
-
 import React, { useEffect, useState } from "react";
 import './ListAllImages.css';
 
